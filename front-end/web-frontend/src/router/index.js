ff34cf7a7e5e211ef1ca91786fd2f9efb0480a03
Yaseen <yaseen.abouhouly@gmail.com>
2026-02-04 16:11:36 +0000

    Add login page

--- front-end/web-frontend/src/router/index.js
+++ front-end/web-frontend/src/router/index.js
@@ -1,25 +1,29 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "Home",
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "/login",
+    name: "Login",
+    component: LoginView
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue")
   }
-]
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
-})
+});
 
-export default router
+export default router;
---
+++ front-end/web-frontend/src/views/LoginView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+
+    <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" />
+    <button>Login</button>
+
+    <button @click="showCreate = !showCreate">
+      Create Account
+    </button>
+
+    <div v-if="showCreate">
+      <h2>Create Account</h2>
+
+      <input type="text" placeholder="Username" />
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button>Create</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+  data() {
+    return {
+      showCreate: false
+    };
+  }
+};
+</script>
+
+<style scoped>
+.login {
+  min-height: 100vh;
+  background: rgb(255, 255, 255);
+  color: rgb(0, 0, 0);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  gap: 10px;
+}
+
+input,
+button {
+  background: rgb(255, 255, 255);
+  color: rgb(0, 0, 0);
+  border: 1px solid rgb(0, 0, 0);
+  padding: 8px;
+  width: 250px;
+}
+</style>
